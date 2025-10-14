42dcc79e0cbeafa52babacb5906d3aee446c9dfd
fokjul <40266249+fokjul@users.noreply.github.com>
2025-10-14 11:32:20 -0700

    adds crn approval link

--- src/utilPages/Home.jsx
+++ src/utilPages/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <li>Introduction</li>
         <li>Components</li>
         <li><Link to='/prototypes/course'>Course Page Prototype</Link></li>
+                <li><Link to='/prototypes/course-crn'>Course Page CRN Approval Prototype</Link></li>
         <li><Link to='/prototypes/cart'>Cart Page Prototype</Link></li>
         <li><Link to='/prototypes/donation'>Donation Page Prototype</Link></li>
       </ul>
